ac797eab4b08762fc87ffdb47f8af31749981fde
Jim Padilla <jp_code2013@yahoo.com>
2017-02-14 11:51:29 -0800

    Question showing

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,4 +1,35 @@
 class QuestionsController < ApplicationController
+  before_action :set_user, only: [:edit, :update, :destroy]
+
+  def show
+    @question = Question.find(params[:id])
+  end
+
   def new
+    @question = Question.new
+  end
+
+  def create
+    @question = @user.questions.new(question_params)
+    if @question.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  private
+  def question_params
+    params.require(:question).permit(:description)
+  end
+
+  def set_user
+    @user = User.find(params[:user_id])
   end
 end
