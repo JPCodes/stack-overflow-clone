d2059d4da8b577d95b0d39bfa58bc3937a0df89f
Jim Padilla <jp_code2013@yahoo.com>
2017-02-14 14:08:06 -0800

    Add errors partial, answers to questions

--- app/controllers/answers_controller.rb
+++ app/controllers/answers_controller.rb
@@ -1,3 +1,23 @@
 class AnswersController < ApplicationController
+  def new
+    @user = User.find(current_user.id)
+    @question = Question.find(params[:id])
+    @answer = @question.answer.new
+  end
 
+  def create
+    @user = User.find(current_user.id)
+    @question = Question.find(params[:question_id])
+    @answer = @question.answers.new(answer_params)
+    if @answer.save
+      redirect_to question_path(@question)
+    else
+      redirect_to question_path(@question)
+    end
+  end
+
+  private
+  def answer_params
+    params.require(:answer).permit(:description, :user_id)
+  end
 end
--- app/models/question.rb
+++ app/models/question.rb
@@ -1,4 +1,5 @@
 class Question < ActiveRecord::Base
+  has_many :answers
   belongs_to :user
   validates :description, :presence => true
 end
