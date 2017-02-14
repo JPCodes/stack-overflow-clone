27d463b79a1facf47aad5149edee41fdff11dd75
Jim Padilla <jp_code2013@yahoo.com>
2017-02-14 14:18:48 -0800

    Debug answers form

--- app/controllers/answers_controller.rb
+++ app/controllers/answers_controller.rb
@@ -6,12 +6,14 @@ class AnswersController < ApplicationController
   end
 
   def create
+    puts "heyheheyheyheyh",current_user
     @user = User.find(current_user.id)
     @question = Question.find(params[:question_id])
     @answer = @question.answers.new(answer_params)
     if @answer.save
       redirect_to question_path(@question)
     else
+      flash[:notice] = "Answer not saved"
       redirect_to question_path(@question)
     end
   end
